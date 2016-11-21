2e652b5a48bc8d7fb51d34d7ce84960eecc97a36
Suvan <suvanramani@gmail.com>
2016-11-21 10:05:08 -0800

    Initial Commit

---
+++ GitHubAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubAPI
+//
+//  Created by Suvan Ramani on 2016-11-21.
+//  Copyright © 2016 suvanr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
